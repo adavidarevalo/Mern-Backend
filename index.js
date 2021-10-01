3ab2e306d35eda671c5959898ec5893e99e5095c
David Arevalo <davidarevaloc20@gmail.com>
2021-10-01 18:12:25 -0500

    resolve the bug

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ app.use(express.json({ extended: true }))
 
 app.use(cors())
 
-const port = process.env.Port || 4000;
+const port = process.env.PORT || 4000;
 
 app.use("/api/user", require("./routes/user"))
 app.use("/api/auth", require("./routes/auth"))
 app.use("/api/projects", require("./routes/projects"))
 app.use("/api/tasks", require("./routes/tasks"))
 
-app.listen(port, ()=>{
+app.listen(process.env.PORT, '0.0.0.0', ()=>{
     console.log(`El servidor esta Corriendo en el puerto ${port}`)
 })
