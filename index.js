96213f88dbf7cb2e62d52c533c9377dba5dc3424
David Arevalo <davidarevaloc20@gmail.com>
2021-10-01 10:51:26 -0500

    Change the cors config

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ const app = express();
 connectDataBase()
 
 app.use(express.json({ extended: true }))
-app.use(cors())
+
+const CorsOptions = {
+    origin: "https://fervent-brown-e592e4.netlify.app/"
+  }
+
+app.use(cors(CorsOptions))
 
 const PORT = process.env.Port || 4000;
 
@@ -16,6 +21,6 @@ app.use("/api/auth", require("./routes/auth"))
 app.use("/api/projects", require("./routes/projects"))
 app.use("/api/tasks", require("./routes/tasks"))
 
-app.listen(PORT, ()=>{
+app.listen(PORT, "0.0.0.0", ()=>{
     console.log(`El servidor esta Corriendo en el puerto ${PORT}`)
 })
